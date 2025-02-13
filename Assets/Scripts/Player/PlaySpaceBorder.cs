1655db86682fcbb513a332a5de68d7882d525821
Crecentez <treegreen2981@gmail.com>
2025-02-13 13:15:37 -0500

    Finished OutOfBounds

--- Assets/Scripts/Player/PlaySpaceBorder.cs
+++ Assets/Scripts/Player/PlaySpaceBorder.cs
@@ -6,9 +6,53 @@ public class PlaySpaceBorder : MonoBehaviour
 {
     public Vector2 SafeZone = Vector2.zero;
     public Vector2 LethalBorder = Vector2.one;
+    public GameObject OutOfBoundsWarning;
 
     [SerializeField]
     private bool showGizmos = false;
+
+    [SerializeField]
+    private float flashTime = 1f;
+
+    private bool showWarning = false;
+
+    private void Update() {
+        if (outOfBounds() && !showWarning) {
+            showWarning = true;
+            startWarning();
+        } else if (!outOfBounds() && showWarning) {
+            showWarning = false;
+            OutOfBoundsWarning.SetActive(false);
+        }
+
+        if (inLethalZone()) {
+            GetComponent<PlayerController>().Kill();
+        }
+    }
+
+    private void startWarning() {
+        OutOfBoundsWarning.SetActive(true);
+        Invoke("warn", flashTime);
+    }
+
+    private void warn() {
+        if (showWarning) {
+            OutOfBoundsWarning.SetActive(!OutOfBoundsWarning.activeInHierarchy);
+            Invoke("warn", flashTime);
+        }
+    }
+
+    private bool outOfBounds() {
+        bool oobX = transform.position.x > (SafeZone.x / 2) || transform.position.x < -(SafeZone.x / 2);
+        bool oobY = transform.position.y > (SafeZone.y / 2) || transform.position.y < -(SafeZone.y / 2);
+        return oobX || oobY;
+    }
+
+    private bool inLethalZone() {
+        bool oobX = transform.position.x > (LethalBorder.x / 2) || transform.position.x < -(LethalBorder.x / 2);
+        bool oobY = transform.position.y > (LethalBorder.y / 2) || transform.position.y < -(LethalBorder.y / 2);
+        return oobX || oobY;
+    }
 
     private void gizmosDrawRectangle(Vector3[] points) {
         for (int i = 0; i < points.Length; i++) {
